18ee324f58077345614e4382a6dc89ddc053750f
Randy Heiland <randy.heiland@gmail.com>
2026-02-07 14:59:47 -0500

    fix title

--- persistent_random_walk/PhysiCell/plot_csv_Nruns.py
+++ persistent_random_walk/PhysiCell/plot_csv_Nruns.py
@@ -8,6 +8,7 @@ import pandas as pd
 csv_file = 'model4_TF.csv'
 csv_file = 'model4_physicell.csv'
 csv_file = 'save_Jan2026.csv'
+csv_file = 'data_Dom.csv'   # Feb 2026
 csv_file = 'physicell.csv'   # Feb 2026
 df = pd.read_csv(csv_file)
 
@@ -21,19 +22,30 @@ y = df['com_2']
 idx0 = 0
 num_per_run = 100  # Jan 2026
 num_per_run = 1000  # Feb 2026
+print('len(x)= ',len(x))
+print('len(x)/num_per_run= ',len(x)/num_per_run)
 N = int(len(x)/num_per_run)
+# N = 1
+#N = 2
+# N = 5
+N = 1000
 print("N= ",N)
 for idx in range(N):
+    # print("idx0= ",idx0)
     # plt.plot(x[idx0:idx0+100],y[idx0:idx0+100], color='gray', linewidth=0.5)
-    plt.plot(x[idx0:idx0+num_per_run],y[idx0:idx0+num_per_run], color='gray', linewidth=0.5)
+    # print("x[idx0:idx0+3]= ",x[idx0:idx0+3])
+    # print("y[idx0:idx0+3]= ",y[idx0:idx0+3])
+    # plt.plot(x[idx0:idx0+num_per_run],y[idx0:idx0+num_per_run], color='gray', linewidth=0.5)
+    plt.plot(x[idx0:idx0+num_per_run],y[idx0:idx0+num_per_run], linewidth=0.5)
     # idx0 += 100
-    idx0 += num_per_run
+    idx0 += num_per_run+1
 
 # Add labels and title
 plt.xlabel('x')
 plt.ylabel('y')
 #plt.title('Persistent cell migration - N runs')
-plt.title(csv_file)
+title = f'{csv_file}, {N} runs'
+plt.title(title)
 
 # Display the plot
 # plt.xlim(40, 700)
